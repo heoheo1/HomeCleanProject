b9b3e6b3d2ce20bcca8fdb3f4cb98ecf2923324d
gjgurwls <hke3255@gmail.com>
2021-06-17 15:15:26 +0900

    Ver(1.3)

--- app/src/main/java/com/hj/homecleanproject/LoginActivity.java
+++ app/src/main/java/com/hj/homecleanproject/LoginActivity.java
@@ -2,7 +2,10 @@ package com.hj.homecleanproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 
 import java.io.File;
@@ -10,25 +13,20 @@ import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
 
-    GridView gridView;
-    ArrayList<MyWork> workList;
-    MyGridAdapter adapter;
-
-    //2021 06 17
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        gridView = findViewById(R.id.grid);
-        workList = new ArrayList<>();
-        adapter = new MyGridAdapter();
-        gridView.setAdapter(adapter);
+        Button button = findViewById(R.id.button);
 
-        adapter.addItem(new MyWork(R.drawable.moon,"하이요"));
-        adapter.addItem(new MyWork(R.drawable.moon,"하이요"));
-        adapter.notifyDataSetChanged();
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(LoginActivity.this,GridActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
