0ac9999a821e56e672d29eabc831ccbde061cda1
gjgurwls <hke3255@gmail.com>
2021-06-20 14:42:11 +0900

    Ver(1.1)

--- app/src/main/java/com/hj/homecleanproject/LoginFragment.java
+++ app/src/main/java/com/hj/homecleanproject/LoginFragment.java
@@ -2,18 +2,11 @@ package com.hj.homecleanproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.GridView;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
